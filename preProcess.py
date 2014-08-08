9d524c85a6d499a489bd47adb537537c6d044d52
Gabriel Pires e Albuquerque de Mello <gabrielmll@gmail.com>
2014-08-08 17:10:55 +0100

    Computed psycolocial symptoms

--- preProcess.py
+++ preProcess.py
@@ -1,6 +1,9 @@
 # Preprocess for statistical analysis
 # data output format:
-#	date[yyyymmdd], id[1-80], gotFlu[0-1], interactions, fluInteractions, healthInteractions,
+#	date[yyyymmdd], id[1-80], gotFlu[0-1], interactions[sum], fluInteractions[sum],
+#	healthInteractions[sum], sore.throat.cough[0-1, runnynose.congestion.sneezing[0-1], fever[0-1],
+#	nausea.vomiting.diarrhea[0-1], gotPsySymp[0-1], psyInteractions[sum], noPsyInteractions[sum],
+#	sadDepressed[0-1], openStressed[0-1]
 
 import csv
 import datetime, time
@@ -30,7 +33,7 @@ try:
 	w = csv.writer(outputFile, delimiter=',');
 	
 	# output File header
-	w.writerows([['date','id','gotFlu','interactions','fluInteractions','healthInteractions','sore.throat.cough','runnynose.congestion.sneezing','fever','nausea.vomiting.diarrhea']])
+	w.writerows([['date','id','gotFlu','interactions','fluInteractions','healthInteractions','sore.throat.cough','runnynose.congestion.sneezing','fever','nausea.vomiting.diarrhea','gotPsySymp','psyInteractions','noPsyInteractions','sadDepressed','openStressed']])
 
 	while int(current_day) != 20090112:
 		# skip the headers
@@ -39,7 +42,7 @@ try:
 
 		print "---------------"
 		print "Date "+str(current_day)
-		# Output variables
+		# Output variables (in order/sorted)
 		gotFlu = [0] * 81	# 0 or 1
 		interactions = [0] * 81	# sum
 		fluInteractions = [0] * 81	# sum
@@ -48,6 +51,11 @@ try:
 		runnynoseCongSneezing = [0] * 81 # 0 or 1
 		fever = [0] * 81 # 0 or 1
 		nauseaVomDiar = [0] * 81 # 0 or 1
+		gotPsySymp = [0] * 81 # 0 or 1
+		psyInteractions = [0] * 81
+		# noPsyInteractions = [0] * 81 Not needed. It's just an subtraction: interactions - psyInteractions
+		sadDepressed = [0] * 81 # 0 or 1
+		openStressed = [0] * 81 # 0 or 1
 
 		# each row of the fluSymptom file
 		# The reason Flu is being analysed first is that after we can easily check the amount of flu interactions
@@ -58,6 +66,7 @@ try:
 
 			# for this day, compute the symptoms
 			if processedDate == int(current_day):
+				# Flu Symps
 				if row[2] == '1':
 					gotFlu[int(row[0])] = 1
 					soreThroatCough[int(row[0])] = 1
@@ -70,6 +79,13 @@ try:
 				if row[5] == '1':
 					gotFlu[int(row[0])] = 1
 					nauseaVomDiar[int(row[0])] = 1
+				# Psy Symps
+				if row[6] == '1':
+					gotPsySymp[int(row[0])] = 1
+					sadDepressed[int(row[0])] = 1
+				if row[7] == '1':
+					gotPsySymp[int(row[0])] = 1
+					openStressed[int(row[0])] = 1
 
 		# each row of the proximity file
 		for row in proximityReader:
@@ -87,6 +103,10 @@ try:
 					fluInteractions[int(row[1])]+=1
 				if gotFlu[int(row[1])] == 1:
 					fluInteractions[int(row[0])]+=1
+				if gotPsySymp[int(row[0])] == 1:
+					psyInteractions[int(row[1])]+=1
+				if gotPsySymp[int(row[1])] == 1:
+					psyInteractions[int(row[0])]+=1
 
 		# pop the 0ith array position
 		interactions.pop(0)
@@ -96,8 +116,12 @@ try:
 		runnynoseCongSneezing.pop(0)
 		fever.pop(0)
 		nauseaVomDiar.pop(0)
+		gotPsySymp.pop(0)
+		psyInteractions.pop(0)
+		sadDepressed.pop(0)
+		openStressed.pop(0)
 		for i in range(len(interactions)):
-			w.writerow([current_day, i+1, gotFlu[i], interactions[i], fluInteractions[i],interactions[i]-fluInteractions[i],soreThroatCough[i],runnynoseCongSneezing[i],fever[i],nauseaVomDiar[i]]);
+			w.writerow([current_day, i+1, gotFlu[i], interactions[i], fluInteractions[i],interactions[i]-fluInteractions[i],soreThroatCough[i],runnynoseCongSneezing[i],fever[i],nauseaVomDiar[i],gotPsySymp[i],psyInteractions[i],interactions[i]-psyInteractions[i],sadDepressed[i],openStressed[i]]);
 
 		current_day = incrementOneDay( current_day );
 		proximityFile.seek(0);
