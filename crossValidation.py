bb6013b0e17fb9f73d2c9495e24ad602be238865
kfarrahi <katayounfarrahi@hotmail.com>
2014-08-31 12:15:02 +0100

    added KNN

--- crossValidation.py
+++ crossValidation.py
@@ -2,9 +2,9 @@ import numpy as np
 from sklearn import cross_validation
 from sklearn import datasets
 from sklearn import svm
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors.nearest_centroid import NearestCentroid
-from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
 
 data = np.loadtxt('./spreadFluFeatures_singleDay.csv', delimiter=',', skiprows=1)
 
@@ -12,19 +12,23 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:, 3:1
 
 # ???
 clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
-y_predicted = clf.predict(X_test)
+clf_y_pred = clf.predict(X_test)
 
-precision = precision_score(y_test,y_predicted)
-recall = recall_score(y_test,y_predicted)
+clf_pr = precision_score(y_test,clf_y_pred)
+clf_rc = recall_score(y_test,clf_y_pred)
+clf_CM = confusion_matrix(y_test,clf_y_pred)
 
 #print clf.score(X_test, y_test)
-
-print y_test
-scores = cross_validation.cross_val_score(clf, data[:, 3:15], data[:, 2], cv=5)
-print scores
+#print y_test
+#scores = cross_validation.cross_val_score(clf, data[:, 3:15], data[:, 2], cv=5)
+#print scores
 
 # Nearest Neighbor
-#nbrs = NearestNeighbors(n_neighbors=2).fit(X_train, y_train)
+nbrs = KNeighborsClassifier(n_neighbors=2).fit(X_train, y_train)
+nbrs_y_pred = nbrs.predict(X_test)
+nbrs_pr = precision_score(y_test,nbrs_y_pred)
+nbrs_rc = recall_score(y_test,nbrs_y_pred)
+nbrs_CM = confusion_matrix(y_test,nbrs_y_pred)
 
 # NearestCentroid
 clf = NearestCentroid().fit(X_train, y_train)
