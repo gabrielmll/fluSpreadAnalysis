f53e5ef733dde2b2bb06f5bdc2eef46060671f86
kfarrahi <katayounfarrahi@hotmail.com>
2014-08-31 12:03:55 +0100

    updated crossvalidation.py

--- crossValidation.py
+++ crossValidation.py
@@ -4,6 +4,7 @@ from sklearn import datasets
 from sklearn import svm
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.metrics import precision_score, recall_score
 
 data = np.loadtxt('./spreadFluFeatures_singleDay.csv', delimiter=',', skiprows=1)
 
@@ -11,7 +12,13 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(data[:, 3:1
 
 # ???
 clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
-print clf.score(X_test, y_test)
+y_predicted = clf.predict(X_test)
+
+precision = precision_score(y_test,y_predicted)
+recall = recall_score(y_test,y_predicted)
+
+#print clf.score(X_test, y_test)
+
 print y_test
 scores = cross_validation.cross_val_score(clf, data[:, 3:15], data[:, 2], cv=5)
 print scores
